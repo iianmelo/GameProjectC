fa3d86319f74d4087da31f035df92eaf257906d0
Luisa Leiria <112179923+luisaleiria@users.noreply.github.com>
2022-10-01 09:20:03 -0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,1 +1,17 @@
+#ifndef menu
+#define menu
 
+#include "menu.h"
+
+void iniciarMenu();
+void abrirExplicacao();
+void iniciandoJogo();
+void paraOndeIr(int aux);
+void fechamento();
+
+//declarando tamanhos da tela
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
+
+
+#endif
