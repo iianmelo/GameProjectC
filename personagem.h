e80ba62dc1927d464f0c41fd6eff5f9abb674acb
Ian Melo <114230401+iianmelo@users.noreply.github.com>
2022-10-07 12:50:16 -0300

    Update personagem.h

--- personagem.h
+++ personagem.h
@@ -1,1 +1,18 @@
+#include "raylib.h"
 
+typedef struct{
+    double velocidade;
+    Rectangle hitboxveiculo;
+    Vector2 posicaoveiculo;
+
+} veiculo;
+
+#include "jogador.h"
+#include <math.h>
+#include  <stdlib.h>
+
+void inicializaCarro(veiculo* carro);
+
+int movimentarCarro1(veiculo* carro1, Rectangle mapa);
+
+int movimentarCarro2(veiculo* carro2, Rectangle mapa);
