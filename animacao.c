acd6d0c3b3ba09c831912758a1bd005e32ce3d5d
Ian Melo <114230401+iianmelo@users.noreply.github.com>
2022-10-07 17:06:27 -0300

    Update animacao.c

--- animacao.c
+++ animacao.c
@@ -1,1 +1,44 @@
+#include "raylib.h"
+#include "animaco.h"
 
+void animarCarro(spritesheet* carro, Texture2D* textura_carro, Texture2D* textura_nitro, veiculo carro){
+
+    if(carro.direcao == 1){ //carro esta indo para cima
+
+        if(carro.nitro == 0){
+
+        }
+        else{ //nitro ligado
+
+        }
+    }
+    else if(carro.direcao == 3){ //carro esta indo para baixo
+
+        if(carro.nitro == 0){
+
+        }
+        else{ //nitro ligado
+
+        }
+    }
+    else if(carro.direcao == 4){ //carro esta indo para esquerda
+
+        if(carro.nitro == 0){
+
+        }
+        else{ //nitro ligado
+
+        }
+    }
+    else if(carro.direcao == 2){ //carro esta indo para direita
+
+        if(carro.nitro == 0){
+
+        }
+        else{ //nitro ligado
+
+        }
+
+    }
+            
+}
