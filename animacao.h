7990097a16fcf1856d4f974fb54f18a25482f409
Ian Melo <114230401+iianmelo@users.noreply.github.com>
2022-10-07 17:06:03 -0300

    Update animacao.h

--- animacao.h
+++ animacao.h
@@ -1,1 +1,16 @@
+#ifndef animacao
+#define animacao
 
+#include "raylib.h"
+#include "animacao.h"
+#include "personagem.h"
+
+typedef struct{
+    int nitro; //se o nitro estiver ligado, fazer a animacao do nitro
+    float angulo; 
+    int direcao; //esse parametro dependera do valor que a funcao de movimentar o carro retornar
+} spritesheetcarro;
+
+void animarCarro(spritesheet* carro, Texture2D* textura_carro, Texture2D* textura_nitro, veiculo carro);
+
+#endif
