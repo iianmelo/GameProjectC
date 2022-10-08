22bb6bec5f89feaa46bed6e3e59b17b70cc24fef
Ian Melo <114230401+iianmelo@users.noreply.github.com>
2022-10-08 19:06:15 -0300

    Update animacao.c

--- animacao.c
+++ animacao.c
@@ -4,34 +4,80 @@
 
 void animarCarro(spritesheetcarro* carro_sheet, Texture2D textura_carro, Texture2D textura_nitro, veiculo carro){
 
-    Vector2 centralizar = {carro.posicaoveiculo.x, carro.posicaoveiculo.y};
+    Vector2 centro_carro = {carro.posicaoveiculo.x, carro.posicaoveiculo.y};
 
     //animar carro caso ele esteja na vertical ou na horizontal: 
     if((*carro_sheet).direcao == 1 || (*carro_sheet).direcao == 2 || (*carro_sheet).direcao == 3 || (*carro_sheet).direcao == 4){
+
         DrawTexture(textura_carro, carro.hitboxveiculo.x, carro.hitboxveiculo.y, RAYWHITE);
+
+        //nitro ligado:
+        if((*carro_sheet).nitro == 1){
+
+            switch((*carro_sheet).direcao){
+                case 1:
+                    Vector2 centro_nitro1 = {centro_carro.x - 19, carro.posicaoveiculo.y + 65};
+                    Vector2 centro_nitro2 = {carro.posicaoveiculo.x + 19, carro.posicaoveiculo.y + 65};
+                    DrawTextureEx(textura_nitro, centro_nitro1, 0.0f, 1.0f, RAYWHITE);
+                    DrawTextureEx(textura_nitro, centro_nitro2, 0.0f, 1.0f, RAYWHITE);
+                    break;
+                case 2:
+                    Vector2 centro_nitro1 = {carro.posicaoveiculo.x - 65, carro.posicaoveiculo.y + 19};
+                    Vector2 centro_nitro2 = {carro.posicaoveiculo.x - 65, carro.posicaoveiculo.y - 19};
+                    DrawTextureEx(textura_nitro, centro_nitro1, 90.0f, 1.0f, RAYWHITE);
+                    DrawTextureEx(textura_nitro, centro_nitro2, 90.0f, 1.0f, RAYWHITE);
+                    break;
+                case 3:
+                    Vector2 centro_nitro1 = {carro.posicaoveiculo.x - 19, carro.posicaoveiculo.y - 65};
+                    Vector2 centro_nitro2 = {carro.posicaoveiculo.x + 19, carro.posicaoveiculo.y - 65};
+                    DrawTextureEx(textura_nitro, centro_nitro1, 180.0f, 1.0f, RAYWHITE);
+                    DrawTextureEx(textura_nitro, centro_nitro2, 180.0f, 1.0f, RAYWHITE);
+                    break;
+                case 4:
+                    Vector2 centro_nitro1 = {carro.posicaoveiculo.x + 65, carro.posicaoveiculo.y + 19};
+                    Vector2 centro_nitro2 = {carro.posicaoveiculo.x + 65, carro.posicaoveiculo.y - 19};
+                    DrawTextureEx(textura_nitro, centro_nitro1, 270.0f, 1.0f, RAYWHITE);
+                    DrawTextureEx(textura_nitro, centro_nitro2, 270.0f, 1.0f, RAYWHITE);
+                    break;
+            }
+        }
     }
 
     //animar carro caso ele esteja na diagonal (parametros mais complexos):
     else if((*carro_sheet).direcao == 5){
-        DrawTextureEx(textura_carro, centralizar, 45.0f , 1.0f, RAYWHITE);
+        DrawTextureEx(textura_carro, centro_carro, 45.0f , 1.0f, RAYWHITE);
+        
+        //nitro ligado:
+        if((*carro_sheet).nitro == 1){
+
+        }
     }
 
     else if((*carro_sheet).direcao == 8){
-        DrawTextureEx(textura_carro, centralizar, 315.0f , 1.0f, RAYWHITE);
+        DrawTextureEx(textura_carro, centro_carro, 315.0f , 1.0f, RAYWHITE);
+
+        //nitro ligado:
+        if((*carro_sheet).nitro == 1){
+            
+        }
     }
 
     else if((*carro_sheet).direcao == 7){
-        DrawTextureEx(textura_carro, centralizar, 45.0f , 1.0f, RAYWHITE);
+        DrawTextureEx(textura_carro, centro_carro, 45.0f , 1.0f, RAYWHITE);
+
+        //nitro ligado:
+        if((*carro_sheet).nitro == 1){
+            
+        }
     }
 
     else if((*carro_sheet).direcao == 6){
-        DrawTextureEx(textura_carro, centralizar, 315.0f , 1.0f, RAYWHITE);
+        DrawTextureEx(textura_carro, centro_carro, 315.0f , 1.0f, RAYWHITE);
+
+        //nitro ligado:
+        if((*carro_sheet).nitro == 1){
+            
+        }
     }
 
-    //animar carro com o nitro ligado: 
-    if((*carro_sheet).nitro == 1){
-
-
-
-    }
 }
