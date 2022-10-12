e0009a82e621c12b0829b626b01bcab3ee2b0531
Ian Melo <114230401+iianmelo@users.noreply.github.com>
2022-10-12 13:41:39 -0300

    Update animacao.h

--- animacao.h
+++ animacao.h
@@ -6,10 +6,20 @@
 #include "personagem.h"
 
 typedef struct{
+    float tempo_duracao;
+} timer;
+
+typedef struct{
     int nitro; //se o nitro estiver ligado, fazer a animacao do nitro
     float angulo; 
     int direcao; //esse parametro dependera do valor que a funcao de movimentar o carro retornar
 } spritesheetcarro;
+
+void startTimer(timer* timer, float tempo_ligado);
+
+void updateTimer(timer* timer);
+
+bool timerDone(timer* timer);
 
 void animarCarro(spritesheetcarro* carro_sheet, Texture2D textura_carro, Texture2D textura_nitro, veiculo carro);
 
