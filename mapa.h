f3f362185b9c15d874c644c6f8b1af03dd9ef607
alicecadete28 <113849722+alicecadete28@users.noreply.github.com>
2022-10-12 16:10:04 -0300

    Update mapa.h

--- mapa.h
+++ mapa.h
@@ -1,1 +1,10 @@
+#include "raylib.h"
+#include "personagem.h"
+#include "animacao.h"
+
+Texture2D abrirMapa();
+
+bool checarLimitesDaCamera();
+
+int colidiu(veiculo carro, Rectangle* mapa, int numero_bordas);
 
