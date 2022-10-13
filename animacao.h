46ffcbef43bef2fc9fc7a82e3351831aa56e794c
OIcaroMelo <114083598+OIcaroMelo@users.noreply.github.com>
2022-10-13 11:39:30 -0300

    Update animacao.h

--- animacao.h
+++ animacao.h
@@ -2,18 +2,8 @@
 #define animacao
 
 #include "raylib.h"
-#include "animacao.h"
-#include "personagem.h"
-
-typedef struct{
-    float tempo_duracao;
-} timer;
-
-typedef struct{
-    int nitro; //se o nitro estiver ligado, fazer a animacao do nitro
-    float angulo; 
-    int direcao; //esse parametro dependera do valor que a funcao de movimentar o carro retornar
-} spritesheetcarro;
+#include "animacao.c"
+#include <stdbool.h>
 
 void startTimer(timer* timer, float tempo_ligado);
 
