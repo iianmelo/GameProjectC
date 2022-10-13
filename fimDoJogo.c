c5bf9562d12f10d2b1a7ce200cf1ccd35fa91427
alicecadete28 <113849722+alicecadete28@users.noreply.github.com>
2022-10-13 19:30:12 -0300

    Create fimDoJogo.c

---
+++ fimDoJogo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "raylib.h"
+#include "menu.h"
+#include "fimDoJogo.h"
+
+void mostrarTelaFinal1(Texture2D imagemVencedor1){
+    
+    imagemVencedor1 = LoadTexture("vencedor1.png");
+    DrawTexture(imagemVencedor1,0,0, RAYWHITE);
+
+    //opcoes que existem nessa tela
+    if(IsKeyDown(KEY_S)){
+    aux=7; //criar uma funcao para sair do jogo
+    }
+    if(IsKeyDown(KEY_H)){
+    aux=3;
+    }
+    if(IsKeyDown(KEY_M)){
+    aux=1
+    }
+}
+void mostrarTelaFinal2(Texture2D imagemVencedor2){
+    imagemVencedor2 = LoadTexture("vencedor2.png");
+    DrawTexture(imagemVencedor2,0,0, RAYWHITE);
+
+    //opcoes que existem nessa tela
+    if(IsKeyDown(KEY_S)){
+    aux=7; //criar uma funcao para sair do jogo
+    }
+    if(IsKeyDown(KEY_H)){
+    aux=3;
+    }
+    if(IsKeyDown(KEY_M)){
+    aux=1
+    }
+}
