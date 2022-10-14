56716248e7892b11e677532e620c183f95798d3a
OIcaroMelo <114083598+OIcaroMelo@users.noreply.github.com>
2022-10-14 01:58:00 -0300

    Update fimDoJogo.c

--- fimDoJogo.c
+++ fimDoJogo.c
@@ -1,36 +1,35 @@
-# include  <stdio.h>
-# include  <raylib.h>
-# include "menu.h"
-# include  "fimDoJogo.h"
+#include <stdio.h>
+#include "raylib.h"
+#include "menu.h"
+#include "fimDoJogo.h"
 
-void mostrarTelaFinal1 (){
-    imagemVencedor1 = LoadTexture ( "vencedor1.png" );
-    DrawTexture (imagemVencedor1, 0 , 0 , RAYWHITE);
-
-    // opções que existem nessa tela
-    if ( IsKeyDown (KEY_S)){
-    aux = 7 ; // cria uma função para sair do jogo
-    }
-    if ( IsKeyDown (KEY_H)){
-    aux = 3 ;
-    }
-    if ( IsKeyDown (KEY_M)){
-    aux = 1;
-    }
-}
-void mostrarTelaFinal2 (){
-    imagemVencedor2 = LoadTexture ( "vencedor2.png" );
-    DrawTexture (imagemVencedor2, 0 , 0 , RAYWHITE);
+void mostrarTelaFinal1(Texture2D imagemVencedor1){
+    imagemVencedor1 = LoadTexture("vencedor1.png");
+    DrawTexture(imagemVencedor1, 0 , 0 , RAYWHITE);
 
     // opções que existem nessa tela
-    if ( IsKeyDown (KEY_S)){
-    aux= 7 ; // cria uma função para sair do jogo
+    if(IsKeyDown(KEY_S)){
+        aux = 8; // cria uma função para sair do jogo
     }
-    if ( IsKeyDown (KEY_H)){
-    aux= 3 ;
+    if(IsKeyDown(KEY_H)){
+        aux = 3;
     }
-    if ( IsKeyDown (KEY_M)){
-    aux = 1;
+    if(IsKeyDown(KEY_M)){
+        aux = 1;
     }
 }
+void mostrarTelaFinal2(Texture2D imagemVencedor2){
+    imagemVencedor2 = LoadTexture("vencedor2.png");
+    DrawTexture(imagemVencedor2, 0 , 0 , RAYWHITE);
 
+    // opções que existem nessa tela
+    if(IsKeyDown(KEY_S)){
+        aux = 8; // cria uma função para sair do jogo
+    }
+    if(IsKeyDown(KEY_H)){
+        aux = 3;
+    }
+    if(IsKeyDown(KEY_M)){
+        aux = 1;
+    }
+}
