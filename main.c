488dfb50489ff2c453e52a5c6e87bafbfdc916a6
OIcaroMelo <114083598+OIcaroMelo@users.noreply.github.com>
2022-10-14 01:56:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,15 +6,16 @@
 #include "raylib.h"
 #include "menu.h"
 #include "mapa.h"
+#include "fimDoJogo.h"
 
 float tempo_nitro_ligado = 2.0; 
 float velocidade_com_nitro = 5;
-float velocidade_base = 2;
 float velocidade_sem_nitro = 2;
 int numero_nitros = 5; //moedas
 int numero_jumpers = 3;
 int acaoCarro1 = 0, Carro1nitro, Carro1moeda = 0;
 int acaoCarro2 = 0, Carro2nitro, Carro2moeda = 0;
+int final = 0;
 
 int main(){
 
@@ -22,14 +23,13 @@ int main(){
     ToggleFullscreen();
     InitAudioDevice();
     SetTargetFPS(60);
-    int ScreenWid = GetScreenWidth();
-    int ScreenHei = GetScreenHeight();
+
     aux=1;
 
     timer timercarro1 = {0, 0};
     timer timercarro2 = {0, 0};
-    spritesheetcarro carro1_sheet = {0, 0.0f, 4, 0}; //nitro comeca desligado e o carro1 nasce apontando para esquerda
-    spritesheetcarro carro2_sheet = {0, 0.0f, 4, 0}; //nitro comeca desligado e o carro2 nasce apontando para esquerda
+    spritesheetcarro carro1_sheet = {0, 0.0f, 4}; //nitro comeca desligado e o carro1 nasce apontando para esquerda
+    spritesheetcarro carro2_sheet = {0, 0.0f, 4}; //nitro comeca desligado e o carro2 nasce apontando para esquerda
 
     //retangulos do mapa:
     Rectangle barreiras[18];
@@ -37,28 +37,30 @@ int main(){
     Rectangle jumpers[numero_jumpers];
 
     //Imagens e Texturas carro 1 (vermelho):
-    Image carro1_cima = LoadImage("C:/Users/Ianme/Downloads/craftpix-889156-free-racing-game-kit/PNG/Car_1_Main_Positions/Car_1_01_cima.png");
+    Image carro1_cima = LoadImage("Car_1_01_cima.png");
     ImageResize (&carro1_cima, 50, 100);
     Texture2D text_carro1_cima = LoadTextureFromImage(carro1_cima);
     
     //Imagens e Texturas carro 2 (amarelo):
-    Image carro2_cima = LoadImage("C:/Users/Ianme/Downloads/craftpix-889156-free-racing-game-kit/PNG/Car_3_Main_Positions/Car_3_01_cima.png");
+    Image carro2_cima = LoadImage("Car_3_01_cima.png");
     ImageResize (&carro2_cima, 50, 100);
     Texture2D text_carro2_cima = LoadTextureFromImage(carro2_cima);
 
     //Imagens e Texturas nitro:
-    Image nitro_imagem = LoadImage("C:/Users/Ianme/Downloads/craftpix-889156-free-racing-game-kit/PNG/Car_Effects/Nitro/Nitro_006.png");
+    Image nitro_imagem = LoadImage("Nitro_006.png");
     ImageResize(&nitro_imagem, 12, 30);
-    Texture2D nit   ro = LoadTextureFromImage(nitro_imagem);
+    Texture2D nitro = LoadTextureFromImage(nitro_imagem);
 
     //Imagens e textura mapa:
-    Image mapa_corrida = LoadImage("C:/Users/Ianme/Downloads/mapa.png");
+    Image mapa_corrida = LoadImage("mapa.png");
     Texture2D mapa_pista = LoadTextureFromImage(mapa_corrida);
 
     //Texturas menu inicial:
-    Texture2D imagemMenu = LoadTexture("C:/Users/Ianme/Downloads/menu_inicial.png");
-    Texture2D imagemComoJogar = LoadTexture("C:/Users/Ianme/Downloads/como_jogar.png");
-    Texture2D imagemIniciando = LoadTexture("C:/Users/Ianme/Downloads/iniciandonovo.png");
+    Texture2D imagemMenu = LoadTexture("menu_inicial.png");
+    Texture2D imagemComoJogar = LoadTexture("como_jogar.png");
+    Texture2D imagemIniciando = LoadTexture("iniciandonovo.png");
+    Texture2D imagemVencedor1 = LoadTexture("vencedor1.png");
+    Texture2D imagemVencedor2 = LoadTexture("vencedor2.png");
 
     ColisaoLimitesPista(barreiras, mapa_pista);
     LocalizaJumper(jumpers, mapa_pista);
@@ -90,14 +92,7 @@ int main(){
 
             if(Carro1moeda != 0) carro1_sheet.moeda = Carro1moeda;
             if(Carro2moeda != 0) carro2_sheet.moeda = Carro1moeda;
-
-            int k1 = carro1_sheet.moeda % 60;
-            velocidade_sem_nitro = k1 * velocidade_base; 
-            int k2 = carro2_sheet.moeda % 60;
-            velocidade_sem_nitro = k2 * velocidade_base;
-
-
-
+            
             //verificando se pegaram nitro:
             Carro1nitro = pegouNitro(carro1, jumpers, numero_jumpers);
             Carro2nitro = pegouNitro(carro2, jumpers, numero_jumpers);
@@ -108,12 +103,12 @@ int main(){
             //caso peguem o nitro, timer comeca e velocidade aumenta
             if(carro1_sheet.nitro == 1 && timercarro1.ligado == 0){
                 startTimer(&timercarro1, tempo_nitro_ligado);
-                carro1.velocidade = 2.5 * velocidade_sem_nitro;
+                carro1.velocidade = velocidade_com_nitro;
                 timercarro1.ligado = 1;
             }    
             if(carro2_sheet.nitro == 1 && timercarro2.ligado == 0){
                 startTimer(&timercarro2, tempo_nitro_ligado);
-                carro2.velocidade = 2.5 * velocidade_sem_nitro;
+                carro2.velocidade = velocidade_com_nitro;
                 timercarro2.ligado = 1;
             }
             
@@ -131,16 +126,36 @@ int main(){
                 carro2_sheet.nitro = 0;
                 timercarro2.ligado = 0;
             }
+            if(CheckCollisionRecs(carro1.hitboxveiculo, linhaDeChegada)) {
+                final = 1;
+                aux = 7;
+                inicializaCarro(&carro1, 1);
+                inicializaCarro(&carro2, 2);
+            }
+            else if(CheckCollisionRecs(carro2.hitboxveiculo, linhaDeChegada)) {
+                final = 2;
+                aux = 7;
+                inicializaCarro(&carro1, 1);
+                inicializaCarro(&carro2, 2);
+            }
+        }
+        if(aux==7 && final==1) {
+            mostrarTelaFinal1(imagemVencedor1);
+        }
+        if(aux==7 && final==2) {
+            mostrarTelaFinal2(imagemVencedor2);
         }
             BeginDrawing();
             ClearBackground(RAYWHITE);
             if(aux==1) {
+                final = 0;
                 iniciarMenu(imagemMenu);
             }
             else if(aux==2){
                 iniciandoJogo(imagemIniciando);
             }
             else if(aux==3){
+                final = 0;
                 abrirExplicacao(imagemComoJogar);
             }
             else if(aux==4){ 
