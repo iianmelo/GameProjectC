d7550bff3b5c891d97111c3c3fab0258dff289b7
OIcaroMelo <114083598+OIcaroMelo@users.noreply.github.com>
2022-10-14 01:57:14 -0300

    Update fimDoJogo.h

--- fimDoJogo.h
+++ fimDoJogo.h
@@ -1,12 +1,12 @@
 #ifndef fimDeJogo
 #define fimDeJogo
 
-#include <raylib.h>
+#include "raylib.h"
 #include "menu.h"
 #include "fimDoJogo.c"
 
-void mostrarTelaFinal1();
+void mostrarTelaFinal1(Texture2D imagemVencedor1);
 
-void mostrarTelaFinal2();
+void mostrarTelaFinal2(Texture2D imagemVencedor2);
 
 #endif
